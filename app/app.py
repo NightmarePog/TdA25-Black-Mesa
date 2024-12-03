bce98ab7880c4897d2540f851990ddc3cb04ccbb
Maferix <valikf22@sosehl.onmicrosoft.com>
2024-12-03 21:42:06 +0100

    Přidán JSON

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ db.init_app(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return "Hello Tour de App!"
+    return {"organization": "Student Cyber Games"}
 
 
 if __name__ == '__main__':
