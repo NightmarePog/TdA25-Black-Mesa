cd4bf39a85f2aa2c47184f3a8e73529fd0ebc762
zeno <zeno.p@seznam.cz>
2025-01-04 20:43:10 +0100

    Upraven start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
-python3 -m flask --app app/app.py init-db
-python3 -m flask --app app/app.py run --host=0.0.0.0 --port=80
+@echo off
+python -m flask --app app\app.py run --host=0.0.0.0 --port=80
+pause
